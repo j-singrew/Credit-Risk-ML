eafe5eb94a74ebbe49035cd10a9cf71ecf53bdeb
topshager <rem28777@gmail.com>
2026-08-13 17:27:24 +0200

    Architecture - Setup architecture and configurations

---
+++ config/appConfig.py
@@ -0,0 +1,9 @@
+import os
+import dotenv
+dotenv.load_dotenv()
+
+class Settings:
+    pass
+
+
+settings = Settings()
---
+++ config/logConfig.py
@@ -0,0 +1,16 @@
+import os 
+import logging
+
+log_file = os.getenv("LOG_FILE_PATH")
+logging.baseConfig(
+    filename = log_file,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+logger.info("Server started")
+
+
+def log_message(message):
+    logger.info(message)
