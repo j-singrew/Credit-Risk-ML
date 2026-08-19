5d6b980aead57ec6f74eb78bf7eb9900fc6e6ba4
topshager <rem28777@gmail.com>
2026-08-19 08:45:40 +0200

    Architecture

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from config.logConfig import logging
 import os
 from dotenv import load_dotenv
+import duckdb
+
+
 
 load_dotenv()
 #from data.__init__ import application_test,application_train,bureau_balance ,bureau,credit_card_balance,home_credit_column,installments,POS_cash,previous_application,sample_submission
 try:    
     application_test = os.getenv("application_test")
-    print(application_test )
-    info_test = pd.read_csv(application_test)
+    duckdb.sql("SELECT 'whistling_duck' AS waterfowl, 'whistle' AS call")
+
 
 except:
     print("nonei")
